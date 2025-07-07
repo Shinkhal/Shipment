33503ea2e1fc92476de6ce377f4fb0e9f7b1328f
Shinkhal <shinkhalsinha@gmail.com>
2025-07-07 10:44:37 +0530

    firebase config fo admin

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,24 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebaseKey.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+
+
+// Decode and parse the base64 string from environment variable
+const firebaseKeyBase64 = process.env.FIREBASE_KEY_BASE64;
+if (!firebaseKeyBase64) {
+  throw new Error("FIREBASE_KEY_BASE64 is not set in environment");
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(firebaseKeyBase64, "base64").toString("utf-8")
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export const db = admin.firestore();
 export const auth = admin.auth();
